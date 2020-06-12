c61877a9c7a49d134cc55e7b461733f864438ec4
Ezwal <ezwal@github.com>
2020-06-12 21:12:46 +0200

    golden winds

--- bridge.clj
+++ bridge.clj
@@ -1,6 +1,6 @@
-(ns bridge
-  (:require [clojure.string :as string]))
-
+(ns Player
+  (:require [clojure.string :as string])
+  (:use clojure.walk))
 
 ; Auto-generated code below aims at helping you parse
 ; the standard input according to the problem statement.
@@ -17,15 +17,16 @@
 
 (def road \.) (def hole \0)
 
-(defn is-path-ok? [lane a b] ;; TODO add error handling to this ?
-  (every? #(= road %)
-        (subs lane a b)))
+(defn is-path-ok? [lane a b]
+    (every? #(= road %)
+            (subs lane a b)))
 
 (defn is-bike-ok? [lanes [o-x o-y] [n-x n-y] jump]
-  (let [starting-pos (if jump n-x (inc o-x))]
+  (let [starting-pos (if jump n-x (inc o-x))
+        ending-pos (bounded-count (inc n-x) (first lanes))] ;; if jump then only check final pos
    (every? true?
-           (list (is-path-ok? (get lanes n-y) starting-pos (inc n-x))
-                 (is-path-ok? (get lanes o-y) starting-pos n-x)))))
+           (list (is-path-ok? (get lanes n-y) starting-pos ending-pos)
+                 (is-path-ok? (get lanes o-y) starting-pos (dec ending-pos))))))
 
 (defn bounded-y [y] (min (max 0 y) 3))
 
@@ -48,56 +49,52 @@
             (map (partial bike-upd lanes new-speed turn (= action "JUMP"))
                  bike-coords)))))
 
-(def actions ["SPEED" "JUMP" "SLOW" "UP" "DOWN" "WAIT"])
+(def actions ["SPEED" "JUMP" "UP" "DOWN"]) ;; remove WAIT and SLOW just as experiment
+
+(def ff (comp first first))
+(defn is-finished? [max-x bikes] (>= (ff bikes) max-x))
+(defn has-minimum-bikes? [minimal-bikes bikes-coords] (>= (count bikes-coords) minimal-bikes))
+(defn is-not-too-long? [actions-list] (< (count actions-list) 50))
 
 ;; defines when the loop cycle actually did it
 (defn win-pred [minimal-bikes max-x bikes-coords]
   (and
    (has-minimum-bikes? minimal-bikes bikes-coords)
    (is-finished? max-x bikes-coords)))
-(defn is-finished? [max-x bikes] (>= (first (first bikes)) max-x))
-(defn has-minimum-bikes? [minimal-bikes bikes-coords] (>= (count bikes-coords) minimal-bikes))
 
-(defn best-sol [lanes max-x speed bikes minimal-bikes & action-list]
-  (if (win-pred minimal-bikes max-x bikes)
-    action-list
-    (for [new-action actions
-          :let [new-bikes (all-bike-upd lanes speed bikes new-action)]]
-      (when (has-minimum-bikes? minimal-bikes new-bikes)
-        (best-sol lanes max-x
-                (+ (action-speed new-action) speed)
-                new-bikes
-                minimal-bikes
-                (conj action-list new-action))))))
+;; (best-sol sample 20 1 [[0 1] [0 2]] 1)
+(defn best-sol
+  ([lanes max-x speed bikes minimal-bikes]
+   (loop [sol (best-sol lanes max-x speed bikes minimal-bikes [])]
+     (if (vector? (first sol))
+       (first sol)
+       (recur (first sol)))))
+  ([lanes max-x speed bikes minimal-bikes action-list]
+   (if (win-pred minimal-bikes max-x bikes)
+     action-list
+     (for [new-action actions
+           :let [new-bikes (all-bike-upd lanes speed bikes new-action)]
+           :when (and (has-minimum-bikes? minimal-bikes new-bikes)
+                      (is-not-too-long? action-list))]
+       (best-sol lanes max-x (+ (action-speed new-action) speed) new-bikes minimal-bikes
+                 (conj action-list new-action))))))
 
 (defn output [msg] (println msg) (flush))
 (defn debug [msg] (binding [*out* *err*] (println msg) (flush)))
 
+(def fc (comp count first))
+(defn read-times [n] (repeatedly n read))
 (defn -main [& args]
-  (let [M (read) V (read) lanes (vec (repeatedly 4 read))]
-    ; M: the amount of motorbikes to control
-    ; V: the minimum amount of motorbikes that must survive
-
-    (let [S (read)] ;; taking initial data
-      (loop [i M data []]
-        (if (= i 0)
-          data
-          (let [X (read) Y (read) A (read)]
-            (recur (dec i)
-                   (cons [X Y] data)))))) ;; idefc about A for the first iteration tbh
-    ;; before
-    (while true
-      (let [S (read)]
-        ; S: the motorbikes' speed
-        (loop [i M]
-          (when (> i 0)
-            (let [X (read) Y (read) A (read)]
-              ; X: x coordinate of the motorbike
-              ; Y: y coordinate of the motorbike
-              ; A: indicates whether the motorbike is activated "1" or detroyed "0"
-            (recur (dec i)))))
-
-        ; (debug "Debug messages...")
-
-        ; A single line containing one of 6 keywords: SPEED, SLOW, JUMP, WAIT, UP, DOWN.
-        (output "SPEED")))))
+  (let [M (read) V (read) lanes [(str (read)) (str (read)) (str (read)) (str (read))]] ;; V: the minimum amount of motorbikes that must survive
+    (debug (type (first lanes)))
+    (let [S (read)
+          bikes (loop [i M data []]
+                  (if (= i 0)
+                    data
+                    (let [X (read) Y (read) A (read)]
+                      (recur (dec i)
+                             (cons [X Y] data)))))
+          sols (best-sol lanes (dec (fc lanes)) S bikes M)] ;; taking initial data
+      (doseq [sol-action sols]
+        (output sol-action)
+        (read-times 4)))))
